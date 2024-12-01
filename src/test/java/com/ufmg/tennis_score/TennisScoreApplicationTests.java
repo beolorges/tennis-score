ef3f549344b41a98e7084a3032d639bbbccebfb5
Leonardo Borges <beolorges@gmail.com>
2024-12-01 15:31:08 -0300

    feat: initial commit

---
+++ src/test/java/com/ufmg/tennis_score/TennisScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufmg.tennis_score;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
